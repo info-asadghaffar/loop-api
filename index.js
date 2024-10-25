69157f1192b62434d7538deda07ec1a0863875d0
info-asadghaffar <r.asadghaffar8800@gmail.com>
2024-10-25 09:04:12 +0500

    Loop Api

---
+++ index.js
@@ -0,0 +1,33 @@
+// // Do-While Loop
+function table() {
+    alert(`Hey Everyone`)
+    let number;
+    do{
+        document.getElementById("number").value;
+    }
+    while((number >= 0 && number < 100)){
+            console.log(`Welcome To ATM`);
+            document.getElementById("show").innerHTML = (`Welcome to ATM`);
+   };
+}
+
+// while Loop
+
+// function table () {
+//     alert(`Check Your Console`)
+//     let number = document.getElementById("number").value;
+//     let i = 1;
+//     while (i<=10){
+//         console.log(`${number} X ${i} = ${number*i}`)
+//         i++;
+//     }
+// }
+
+// For Loop 
+// function table () {
+//     alert(`Check Your Console`)
+//     let number = document.getElementById("number").value ;
+//     for (let i = 1; i<=10 ; i++){
+//         console.log(`${number} X ${i} = ${number*i}`)
+//     }
+// }
